3dd832b9c551cd91016cc91193cb6d2290b1d42e
Al2206 <77494086+Al2206@users.noreply.github.com>
2021-10-08 18:07:45 +0300

    Add files via upload

---
+++ Variables Bolt/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int x = 10;// первый и быстрый способ
+    int x1;
+    x1=10;//простой способ
+        final int LIMIT =5;
+        int a,b,c,d,e,f;
+        a=1;
+        b=a+2;
+        c=b+7;
+        d=10;
+        e=d/2;
+        f=c*30;
+    System.out.print(f);
+    }
+}
+
